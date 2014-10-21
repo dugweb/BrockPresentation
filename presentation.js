f721594953945717c4b5b2cacbcc367b2ad10cb3
Doug Webster <dugweb@gmail.com>
2014-10-20 18:07:49 -0600

    Basic layout up there
    Just flesh out the slides now

--- presentation.js
+++ presentation.js
@@ -3,13 +3,21 @@ var main = angular.module('presentation', ['ngRoute', 'ngAnimate']);
 main.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 
 	$routeProvider
-		.when('/', {
+		.when('/1', {
 			templateUrl: 'slides/1-welcome.html',
 			controller: 'slideController'
 		})
 		.when('/2', {
-			templateUrl: 'slides/2-slide2.html',
+			templateUrl: 'slides/2-AvailableMoney.html',
 			controller:'slideController'
+		})
+		.when('/3', {
+			templateUrl: 'slides/3-Time.html',
+			controller: 'slideController'
+		})
+		.when('/4', {
+			templateUrl: 'slides/4-Planning.html',
+			controller: 'slideController'
 		});
 
 }]);
